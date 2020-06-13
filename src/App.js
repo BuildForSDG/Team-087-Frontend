86f28f6a585deab851867370471d2c9918fbb146
Emma NWAMAIFE <emadimabua@gmail.com>
2020-06-13 16:04:41 +0100

    chore(security): add authentication to routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import {createMuiTheme, MuiThemeProvider} from '@material-ui/core';
 
 import Signup from './components/auth/Signup';
@@ -8,6 +8,19 @@ import NotFound from './components/NotFound';
 import UsersList from './components/users/List';
 import DashBoard from './components/DashBoard';
 import Recommedation from './components/users/Recommendation';
+import { isLoggedIn } from './helpers';
+
+/**
+ *
+ * @see https://jasonwatmore.com/post/2017/09/16/react-redux-user-registration-and-login-tutorial-example
+ * @see https://medium.com/the-andela-way/understanding-the-fundamentals-of-routing-in-react-b29f806b157e
+ */
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={
+    props => (isLoggedIn ? <Component {...props} /> : <Redirect to={{ pathname: '/', state: { from: props.location } }} />)
+  }
+  />
+);
 
 const THEME = createMuiTheme ({
   typography: {
@@ -21,11 +34,11 @@ function App () {
       <Router>
         <Switch>
           <Route exact path="/" component={Login} />
-          <Route exact path="/dashboard" component={DashBoard} />
-          <Route exact path="/neighbourhood-experts" component={Recommedation} />
+          <PrivateRoute exact path="/dashboard" component={DashBoard} />
+          <PrivateRoute exact path="/neighbourhood-experts" component={Recommedation} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/join" component={Signup} />
-          <Route exact path="/users" component={UsersList} />
+          <PrivateRoute exact path="/users" component={UsersList} />
           <Route path="*" component={NotFound} />
         </Switch>
       </Router>
